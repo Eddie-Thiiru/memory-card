3c5a657abdb8f707afc747e064830cda3ad9a66e
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-07-17 16:06:52 +0300

    Add scoreboard and update layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Dalinar from "./components/Dalinar";
 import Jorg from "./components/Jorg";
 import Kaladin from "./components/Kaladin";
@@ -13,24 +13,42 @@ import "./styles/App.css";
 import "./styles/Cards.css";
 
 const App = () => {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [dalinar, setDalinar] = useState({ hit: false });
+  const [jorg, setJorg] = useState({ hit: false });
+  const [kaladin, setKaladin] = useState({ hit: false });
+  const [kelsier, setKelsier] = useState({ hit: false });
+  const [kvothe, setKvothe] = useState({ hit: false });
+  const [logen, setLogen] = useState({ hit: false });
+  const [mat, setMat] = useState({ hit: false });
+  const [rand, setRand] = useState({ hit: false });
+  const [geralt, setGeralt] = useState({ hit: false });
+  const [vin, setVin] = useState({ hit: false });
+
   return (
     <div className="App">
       <div className="header">
         <h1>Hello</h1>
       </div>
       <div className="mainSection">
-        <Dalinar />
-        <Jorg />
-        <Kaladin />
-        <Kelsier />
-        <Kvothe />
-        <Logen />
-        <Mat />
-        <Rand />
-        <Vin />
-        <Geralt />
+        <div className="scoreBoard">
+          <div>Current Score: {score}</div>
+          <div>Best Score: {bestScore}</div>
+        </div>
+        <div className="playGround">
+          <Dalinar />
+          <Jorg />
+          <Kaladin />
+          <Kelsier />
+          <Kvothe />
+          <Logen />
+          <Mat />
+          <Rand />
+          <Geralt />
+          <Vin />
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
