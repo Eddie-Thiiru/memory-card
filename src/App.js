8b9ce610c7e5fcd7d5e807363adcbb05306c8d5a
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-07-19 08:02:22 +0300

    Add randomizer
    This displays the cards in a random order anytime a user clicks one.
    Moreover, the randomizer is also invoked when the component mounts.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Dalinar from "./components/Dalinar";
 import Jorg from "./components/Jorg";
 import Kaladin from "./components/Kaladin";
@@ -9,13 +9,28 @@ import Mat from "./components/Mat";
 import Rand from "./components/Rand";
 import Vin from "./components/Vin";
 import Geralt from "./components/Geralt";
+import uniqid from "uniqid";
 import "./styles/App.css";
 import "./styles/Cards.css";
 
+let initialComponents = [
+  Dalinar,
+  Jorg,
+  Kaladin,
+  Kelsier,
+  Kvothe,
+  Logen,
+  Mat,
+  Rand,
+  Vin,
+  Geralt,
+];
+
 const App = () => {
+  const [components, setComponents] = useState(initialComponents);
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
-  const [hitStatus, setHitStatus] = useState([
+  const [characters, setCharacters] = useState([
     { name: "dalinar", hit: false },
     { name: "jorg", hit: false },
     { name: "kaladin", hit: false },
@@ -28,18 +43,28 @@ const App = () => {
     { name: "vin", hit: false },
   ]);
 
+  useEffect(() => {
+    randomizeCards();
+  }, []);
+
   const handleClicks = (e) => {
+    addScore(e);
+    randomizeCards();
+  };
+
+  const addScore = (e) => {
     const characterName = e.target.id;
     let gameOver = false;
 
-    setHitStatus(
-      hitStatus.map((item) => {
+    setCharacters(
+      characters.map((item) => {
         if (item.name === characterName) {
           if (item.hit === false) {
             setScore(score + 1);
             return { ...item, hit: true };
           } else {
             gameOver = true;
+            return item;
           }
         } else {
           return item;
@@ -52,14 +77,45 @@ const App = () => {
 
       setScore(0);
       setBestScore(value);
-      setHitStatus(
-        hitStatus.map((item) => {
+      setCharacters(
+        characters.map((item) => {
           return { ...item, hit: false };
         })
       );
 
       gameOver = false;
     }
+  };
+
+  const randomizeCards = () => {
+    const isNumVisited = (array, num) => {
+      for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+
+        if (element === num) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+    let randomComponents = [];
+    let visited = [];
+    let copy = [...components];
+
+    while (randomComponents.length < 10) {
+      let randomNum = Math.floor(Math.random() * 10);
+      const numVisited = isNumVisited(visited, randomNum);
+
+      if (numVisited === false) {
+        let component = copy[randomNum];
+
+        randomComponents.push(component);
+        visited.push(randomNum);
+      }
+    }
+
+    setComponents(randomComponents);
   };
 
   return (
@@ -73,16 +129,11 @@ const App = () => {
           <div>Best Score: {bestScore}</div>
         </div>
         <div className="playGround">
-          <Dalinar cardClicked={handleClicks} />
-          <Jorg cardClicked={handleClicks} />
-          <Kaladin cardClicked={handleClicks} />
-          <Kelsier cardClicked={handleClicks} />
-          <Kvothe cardClicked={handleClicks} />
-          <Logen cardClicked={handleClicks} />
-          <Mat cardClicked={handleClicks} />
-          <Rand cardClicked={handleClicks} />
-          <Geralt cardClicked={handleClicks} />
-          <Vin cardClicked={handleClicks} />
+          {components.map((item) => {
+            let Component = item;
+            let id = uniqid();
+            return <Component key={id} cardClicked={handleClicks} />;
+          })}
         </div>
       </div>
     </div>
